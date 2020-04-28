c46aa951ae74efb2e64c5f7c4e337fce9b0c7790
Martin van Zyl <martin.vanzyl45@gmail.com>
2020-04-28 15:46:27 +0200

    Initial dev commit

--- src/main/java/guru/springframework/spring5recipeapp/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/spring5recipeapp/Spring5RecipeAppApplication.java
@@ -1,4 +1,4 @@
-package guru.springframework;
+package guru.springframework.spring5recipeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,4 +9,5 @@ public class Spring5RecipeAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5RecipeAppApplication.class, args);
 	}
+
 }
